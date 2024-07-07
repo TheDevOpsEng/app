02b4035594781ea3907f7388cfe6c4500dac5c4c
TheDevOpsEng <irfan.rafiqueus@gmail.com>
2024-07-06 22:18:32 -0400

    Updates

---
+++ precache-manifest.f94f19b9694155a1526fbac7422a01e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1ae14261a4decf8cf7ddc76003eee5b",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "4b0c9ead3f0410c18b7e",
+    "url": "/app/static/css/main.6710643c.chunk.css"
+  },
+  {
+    "revision": "1d10ea9b931de22288ec",
+    "url": "/app/static/js/2.8df5d3da.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/app/static/js/2.8df5d3da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0439ad82944cf89f18b",
+    "url": "/app/static/js/3.3fcc1e15.chunk.js"
+  },
+  {
+    "revision": "4b0c9ead3f0410c18b7e",
+    "url": "/app/static/js/main.6bf041d3.chunk.js"
+  },
+  {
+    "revision": "52abd7e5786824b7f784",
+    "url": "/app/static/js/runtime-main.bca52c48.js"
+  }
+]);
